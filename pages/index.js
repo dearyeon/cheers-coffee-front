27afdce9062cc8576d5b7930e297d8e6d8786e9a
ImInnocent <ssje2006@naver.com>
2022-04-22 20:54:20 +0900

    Add comments in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,92 @@
-import styles from '../styles/Home.module.css'
-import { useEffect, useState } from 'react'
-import useSWR from 'swr'
+import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
-import { useWeb3React } from '@web3-react/core'
-import { InjectedConnector } from '@web3-react/injected-connector'
+import { useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
 
 const ConnectWallet = () => {
-  const injectedConnector = new InjectedConnector({supportedChainIds: [1,3, 4, 5, 42, ],})
-  const { chainId, account, activate, active,library } = useWeb3React()
-  const [balance,setBalance]= useState("")
-  
+  // supportedChainIds: 접속할 서버들
+  // https://besu.hyperledger.org/en/stable/Concepts/NetworkID-And-ChainID/
+  // 1: Mainnet (Production), 3: Ropsten (Test), 4: Rinkeby (Test), 5: Georli (Test), 42: Kovan (Test)
+  const injectedConnector = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] });
+
+  // useWeb3React: @web3-react에서 제공하는 훅
+  // chainId: 현재 지갑에 연결된 체인 Id
+  // account: 연결된 계좌 주소
+  // activate: 연결 실행 함수
+  // active: 현재 연결된 상태인지 표시
+  // library: 여러가지 함수를 호출할 수 있는 플러그인
+  const { chainId, account, activate, active, library } = useWeb3React();
+
+  // balance: 현재 자산을 기록하는 스테이트.
+  const [balance, setBalance] = useState("");
+
   const onClick = () => {
-    activate(injectedConnector)
-  }
+    // 버튼 클릭 시, 지갑 연결
+    activate(injectedConnector);
+  };
 
   useEffect(() => {
-    console.log(chainId, account, active)
-    },);
+    // 스테이트 업데이트 될 때마다 상태 로깅
+    console.log(chainId, account, active);
+  });
 
   useEffect(() => {
-    library?.getBalance(account).then((result)=>{
-      setBalance(result/1e18)
-    })
-    },);
+    // 주기적으로 잔고를 가져옴.
+    library?.getBalance(account).then((result) => {
+      setBalance(result / 1e18);
+    });
+  });
 
-    const fetcher = (library) => (...args) => {
-      const [method, ...params] = args
-      console.log("fetcher",method, params)
-      const result = library[method](...params)
-     return library[method](...params)
+  // SWR을 이용해 패칭할 때 이용할 패쳐
+  const fetcher = (library) => (...args) => {
+    // ['getBalance', account, 'latest']
+    // getBalance -> method
+    // account, 'latest' -> args
+    // getBalance(account, 'latest');
+    const [method, ...params] = args;
+
+    console.log("fetcher", method, params);
+
+    const result = library[method](...params);
+    return result;
+  };
+
+  const BalanceSWR = () => {
+    const { account, library } = useWeb3React();
+    // data로 받았지만 balance로 이름을 바꿔서 사용
+    // mutate: ??? 조사 필요
+    const { data: balance, mutate } = useSWR(['getBalance', account, 'latest'], {
+      fetcher: fetcher(library),
+    });
+
+    useEffect(() => {
+      // listen for changes on an Ethereum address
+      console.log(`listening for blocks...`);
+
+      // 블록이 업데이트 되면 (새로운 블록이 생기면) 자산을 업데이트 함.
+      library.on('block', () => {
+        console.log('update balance...');
+        mutate(undefined, true);
+      });
+
+      // remove listener when the component is unmounted
+      return () => {
+        library.removeAllListeners('block');
+      };
+
+      // trigger the effect only on component mount
+    }, []);
+
+    console.log(balance);
+
+    if (!balance) {
+      return <div>...</div>;
     }
-
-    const BalanceSWR  = () => {
-      const { account, library } = useWeb3React()
-      const { data: balance, mutate  } = useSWR(['getBalance', account, 'latest'], {
-        fetcher: fetcher(library),
-      })
-      useEffect(() => {
-        // listen for changes on an Ethereum address
-        console.log(`listening for blocks...`)
-        library.on('block', () => {
-          console.log('update balance...')
-          mutate(undefined, true)
-        })
-        // remove listener when the component is unmounted
-        return () => {
-          library.removeAllListeners('block')
-        }
-        // trigger the effect only on component mount
-      }, [])
-
-      console.log(balance)
-      if(!balance) {
-        return <div>...</div>
-      }
-      return <div>Balance: Ξ {balance/1e18}</div>
-    }  
+    
+    return <div>Balance: Ξ {balance / 1e18}</div>;
+  }
 
   return (
     <div>
@@ -71,18 +102,18 @@ const ConnectWallet = () => {
         </button>
       )}
     </div>
-  )
-}
+  );
+};
 
 const Home = () => {
   return (
-    <div >
+    <div>
       <main className={styles.main}>
-        <h2 >Welcome to playground</h2>
+        <h2>Welcome to playground</h2>
         <ConnectWallet />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
