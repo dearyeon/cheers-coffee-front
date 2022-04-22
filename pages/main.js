3bf2584cdba68062cd5463e594a4a9da59ad564d
dearyeon <d_earyeon@naver.com>
2022-04-22 21:26:06 +0900

    fullPage scroll

---
+++ pages/main.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Button from "../components/Button";
+
+function Main() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div className="wrapper">
+      <div className="container" style={{ background: "lightgray" }}>
+        <div className="main-imgboxWrapper">
+          <div className="main-imgbox">
+            <Button text="커피로 응원하기" />
+          </div>
+        </div>
+      </div>
+      <div className="container">test</div>
+    </div>
+  );
+}
+
+export default Main;
