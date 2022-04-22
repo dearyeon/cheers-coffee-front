eb8d9d40ec1ba31dada9af1e28d71504bdd02a1c
ImInnocent <ssje2006@naver.com>
2022-04-22 20:53:59 +0900

    Apply Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,26 @@
 import '../styles/globals.css'
 
+import Head from 'next/head';
 import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 
 function getLibrary(provider) {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 12000
-  return library
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
 }
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
-  )
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+      </Web3ReactProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
