28416547ea79e633063b230422822cfbd03ae120
dearyeon <d_earyeon@naver.com>
2022-04-25 20:16:52 +0900

    main화면 중간push

--- pages/main.js
+++ pages/main.js
@@ -7,8 +7,15 @@ function Main() {
   return (
     <div className="wrapper">
       <div className="container" style={{ background: "lightgray" }}>
-        <div className="main-imgboxWrapper">
-          <div className="main-imgbox">
+        <div className="main-contentWrapper">
+          <div className="main-ment">
+            Oh my... 지친 개발자 {count}에게
+            <br />
+            커피를 사주는 당신의 눈동자에
+            <br />치 ★ 얼 ☆ 쓰 -
+          </div>
+          <div className="main-content">
+            <div className="main-imgBox" />
             <Button text="커피로 응원하기" />
           </div>
         </div>
