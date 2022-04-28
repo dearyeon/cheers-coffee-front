22b86df7c681eebd1b5fcd0d92a7431dca50c680
ImInnocent <ssje2006@naver.com>
2022-04-28 19:41:31 +0900

    노트북 -> PC 임시 저장

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,8 @@ import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-import { useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector';
-import { ethers } from 'ethers';
-import donateABI from '../abi/Donate_abi.json';
 
 const ConnectWallet = () => {
-  // supportedChainIds: 접속할 서버들
-  // https://besu.hyperledger.org/en/stable/Concepts/NetworkID-And-ChainID/
-  // 1: Mainnet (Production), 3: Ropsten (Test), 4: Rinkeby (Test), 5: Georli (Test), 42: Kovan (Test)
-  const injectedConnector = new InjectedConnector({ supportedChainIds: [1, 3] });
-
-  // useWeb3React: @web3-react에서 제공하는 훅
-  // chainId: 현재 지갑에 연결된 체인 Id
-  // account: 연결된 계좌 주소
-  // activate: 연결 실행 함수
-  // active: 현재 연결된 상태인지 표시
-  // library: 여러가지 함수를 호출할 수 있는 플러그인
-  const { chainId, account, activate, active, library } = useWeb3React();
-
   // balance: 현재 자산을 기록하는 스테이트.
   const [balance, setBalance] = useState("");
 
@@ -90,32 +73,6 @@ const ConnectWallet = () => {
     return <div>Balance: Ξ {balance / 1e18}</div>;
   }
 
-  const handleClickDonate = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        console.log(donateABI)
-        const contract = new ethers.Contract(
-          "0x644b70F2E6Bf58B27B5aD5C2bbdd496Ba4482226", 
-          donateABI.abi,
-          library
-        );
-
-        console.log("Initialize");
-        let txn = await contract.getRegistered("goldfish", {});
-
-        // console.log("waiting...");
-        // await txn.wait();
-
-        console.log("mining ended");
-        console.log(txn);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
   return (
     <div>
       <div>ChainId: {chainId}</div>
@@ -129,9 +86,9 @@ const ConnectWallet = () => {
           Connect Connect
         </button>
       )}
-      <button onClick={handleClickDonate}>
+      {/* <button onClick={handleClickDonate}>
         Call Donate
-      </button>
+      </button> */}
     </div>
   );
 };
