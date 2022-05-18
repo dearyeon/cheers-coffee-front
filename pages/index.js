e2f53c7e0e6da586a69842ecb26ec3196d68ef8c
dearyeon <d_earyeon@naver.com>
2022-05-18 21:10:23 +0900

    .

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import * as React from "react";
+import { useRouter } from "next/router";
 import WrapContainer from "../components/common/WrapContainer";
 import Button from "../components/common/Button";
 
 function Register() {
+  const router = useRouter();
   const [isAble, setIsAble] = React.useState(false);
   const [id, setId] = React.useState("");
   const [ment, setMent] = React.useState("");
@@ -10,13 +12,13 @@ function Register() {
   const checkingConditon = () => {
     const regex = /^[a-z0-9+]{2,8}$/;
     if (regex.test(id)) {
-      setMent("");
       setIsAble(true);
+      setMent("사용 가능한 아이디입니다.");
     } else setMent("2~8자의 영어 소문자, 숫자만 사용 가능합니다.");
   };
   const checkingUser = () => {
     if (!isAble) setMent("중복을 체크해주세요.");
-    //else => 가입완료
+    else router.push(`/user/${id}`);
   };
 
   return (
@@ -34,7 +36,7 @@ function Register() {
         />
         <Button text="중복조회" className="radius" onClick={checkingConditon} />
       </div>
-      <div className="text-red">{ment}</div>
+      <div className={isAble ? "text-blue" : "text-red"}>{ment}</div>
       <Button
         text="등록하기"
         className="join-button radius"
